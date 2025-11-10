ad71ff5404c42ff14ddfd466b70a685d4b814412
badgerttl <busboys.corbel0b@icloud.com>
2025-11-10 09:20:56 -0500

    localhost only

--- app.py
+++ app.py
@@ -307,4 +307,4 @@ def index():
 # Run only when executed directly (python app.py)
 # ------------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    app.run(host="127.0.0.1", port=5001, debug=False)
